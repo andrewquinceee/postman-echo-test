bf2050e8236f7c86885f666c4ae6bc9075710435
neto test <andrewquinceee@gmail.com>
2026-07-09 10:56:42 +0300

    Initial commit

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnSentData() {
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data")); // <-- ВОТ ЭТО МЫ ДОБАВИЛИ
+    }
+}
